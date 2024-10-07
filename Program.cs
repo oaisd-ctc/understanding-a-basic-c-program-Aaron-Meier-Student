7e57992aba18988863fbea2d352ab5e79505f371
Aaron-Meier-Student <aamea72@student.oaisd.org>
2024-10-07 12:55:44 +0000

    Created cs file and added basic code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("My name is Aaron Meier.");
+
+        //Using Variables
+        int age = 18;
+        string greeting = "Hello World";
+        string firstname = "Aaron";
+        string lastname = "Meier";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+}
